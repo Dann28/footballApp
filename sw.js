3582627e9f29b6dd9eac290897ee0989689373cd
DSSPM1 <stensen.daniel@spiromagic.dk>
2026-05-05 15:13:08 +0200

    bench popup, drag and drop/swap logic, v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fb-v10';
+const CACHE='fb-v12';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
